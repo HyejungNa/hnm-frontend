929072144d4a1afdd43172b588ea94ee06da1f6f
Hyejung Na <hyejung.na01@gmail.com>
2024-10-31 04:08:14 +1100

    Implement search query and product fetching logic

--- src/page/AdminProductPage/AdminProductPage.js
+++ src/page/AdminProductPage/AdminProductPage.js
@@ -38,11 +38,23 @@ const AdminProductPage = () => {
 
   //상품리스트 가져오기 (url쿼리 맞춰서) - 페이지 들어오자마자 보여줌(useEffect)
   useEffect(() => {
-    dispatch(getProductList());
-  }, []);
+    dispatch(getProductList({ ...searchQuery }));
+  }, [query]);
+
+  // useEffect(() => {
+  //   dispatch(getProductList());
+  // }, []);
 
   useEffect(() => {
     //검색어나 페이지가 바뀌면 url바꿔주기 (검색어또는 페이지가 바뀜 => url 바꿔줌=> url쿼리 읽어옴=> 이 쿼리값 맞춰서  상품리스트 가져오기)
+    if (searchQuery.name === "") {
+      delete searchQuery.name;
+    }
+    console.log("qqq", searchQuery);
+    const params = new URLSearchParams(searchQuery);
+    const query = params.toString();
+    console.log("qqq", query);
+    navigate("?" + query);
   }, [searchQuery]);
 
   const deleteItem = (id) => {
