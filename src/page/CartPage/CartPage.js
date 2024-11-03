bb62ee4763dd1ea033106fce11d2d6dfe4a6493f
Hyejung Na <hyejung.na01@gmail.com>
2024-11-03 22:34:17 +1100

    Fetch cart list and display total price on cart page

--- src/page/CartPage/CartPage.js
+++ src/page/CartPage/CartPage.js
@@ -13,6 +13,7 @@ const CartPage = () => {
 
   useEffect(() => {
     //카트리스트 불러오기
+    dispatch(getCartList());
   }, []);
 
   return (
@@ -25,13 +26,13 @@ const CartPage = () => {
             ))
           ) : (
             <div className="text-align-center empty-bag">
-              <h2>카트가 비어있습니다.</h2>
-              <div>상품을 담아주세요!</div>
+              <h2>Your cart is empty.</h2>
+              <div>Please add items!</div>
             </div>
           )}
         </Col>
         <Col xs={12} md={5}>
-          <OrderReceipt />
+          <OrderReceipt cartList={cartList} totalPrice={totalPrice} />
         </Col>
       </Row>
     </Container>
