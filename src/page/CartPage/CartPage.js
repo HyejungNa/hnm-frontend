b2a2fcee11f61bb2d4b90864c938ac5ec6cefac6
Hyejung Na <hyejung.na01@gmail.com>
2024-11-08 06:32:38 +1100

    revert: Remove stock filter from cart page and order summary

--- src/page/CartPage/CartPage.js
+++ src/page/CartPage/CartPage.js
@@ -14,7 +14,18 @@ const CartPage = () => {
   useEffect(() => {
     //카트리스트 불러오기
     dispatch(getCartList());
-  }, []);
+  }, [dispatch]);
+
+  // 재고가 0개 이상인 아이템만 카트페이지에 보이도록 수정 (원래는 주문시 재고가 0이되어도 카트아이템이랑, order summary에 같이 보였음)
+  // const filteredCartList = cartList.filter(
+  //   (item) => item.productId.stock[item.size] > 0
+  // );
+
+  // // Calculate the updated totalPrice based on filteredCartList
+  // const filteredTotalPrice = filteredCartList.reduce(
+  //   (acc, item) => acc + item.productId.price * item.qty,
+  //   0
+  // );
 
   return (
     <Container>
