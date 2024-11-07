c6c5aa2d0e77135d060af6747142fa7aacac6f9d
Hyejung Na <hyejung.na01@gmail.com>
2024-11-08 06:31:16 +1100

    Translate Korean UI text to English

--- src/page/AdminOrderPage/component/OrderTable.js
+++ src/page/AdminOrderPage/component/OrderTable.js
@@ -25,7 +25,8 @@ const OrderTable = ({ header, data, openEditForm }) => {
                 {item.items.length > 0 ? (
                   <th>
                     {item.items[0].productId.name}
-                    {item.items.length > 1 && `외 ${item.items.length - 1}개`}
+                    {item.items.length > 1 &&
+                      ` and ${item.items.length - 1} more`}
                   </th>
                 ) : (
                   <th></th>
